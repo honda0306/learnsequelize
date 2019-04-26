f373a257ecd855cea648e42ab743a68aa0fb2aac
Chris Honda <christopher.honda92@gmail.com>
2019-04-26 15:44:44 -0700

    Apped endpoint for blog POST

--- index.js
+++ index.js
@@ -6,17 +6,31 @@ const { User, Blog, Tag } = require('./sequelize');
 const app = express();
 app.use(bodyParser.json());
 
+// Retrieve all users
+app.get('/api/users', (req, res) => {
+    User.findAll()
+        .then(users => res.json(users));
+});
+
 // Create user
 app.post('/api/users', (req, res) => {
     User.create(req.body)
         .then(user => res.json(user));
 });
 
-// Retrieve all users
-app.get('/api/users', (req, res) => {
-    User.findAll()
-        .then(users => res.json(users));
-;})
+// Create a blog post
+app.post('/api/blogs', (req, res) => {
+    const body = req.body;
+    const tags = body.tags.map(tag => Tag.findOrCreate({ where: { name: tag.name }, defaults: { name: tag.name }})
+        .spread((tag, created) => tag));
+
+    User.findById(body.userId)
+        .then(() => Blog.create(body))
+        .then(blog => Promise.all(tags).then(storedTags => blog.addTags(storedTags)).then(() => blog))
+        .then(blog => Blog.findOne({ where: {id: blog.id}, include: [User, Tag]}))
+        .then(blogWithAssociations => res.json(blogWithAssociations))
+        .catch(err => res.status(400).json({ err: `User with id = [${body.userId}] doesn\'t exist.`}))
+});
 
 const port = 3000;
 
