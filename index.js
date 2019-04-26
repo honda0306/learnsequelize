69a67ee9de4b1350ee947b94decf6a42e7fa5c75
Chris Honda <christopher.honda92@gmail.com>
2019-04-26 15:31:45 -0700

    Added GET/POST for users

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const { User, Blog, Tag } = require('./sequelize');
+
 const app = express();
 app.use(bodyParser.json());
+
+// Create user
+app.post('/api/users', (req, res) => {
+    User.create(req.body)
+        .then(user => res.json(user));
+});
+
+// Retrieve all users
+app.get('/api/users', (req, res) => {
+    User.findAll()
+        .then(users => res.json(users));
+;})
 
 const port = 3000;
 
